cdeeea6816b78725bb40edbb06002ef7144339e5
Agababov_Maxim <agabm546@hsd.k12.or.us>
2023-12-05 13:52:20 -0800

    added all buttons

--- main.py
+++ main.py
@@ -7,37 +7,99 @@ name of the next card.
 from pypercard import App, Card
 
 
-def lose(app, card):
-    if card == "card2":
-        return "card3"
+#def lose(app, card):
+#    if card == "card2":
+#        return "card3"
 
 
 cards = [
-    Card("start"),
-    Card("bombexploding"),
-    Card("timerjump"),
+    Card("start", auto_advance=10, transition="lose"),
+    Card("bombexploding", auto_advance=8, transition="lose"),
+    Card("timerjump", auto_advance=5, transition="lose"),
     Card("grounded"),
     Card("mad"),
     Card("ok"),
     Card("voltage"),
     Card("parry"),
     Card("lake"),
-    Card("cops")
+    Card("cops"),
+    Card("lose", auto_advance=3.5, transition="start")
 ]
 
-# , auto_advance=10, transition="card2"
+# , auto_advance=10, transition="card2" or function name
 
 carousel_app = App(
     name="PyperCard carousel", datastore={"counter": 0}, cards=cards
 )
 
-@carousel_app.transition("start", "click", "progress")
+#---------------------- Start
 
-def reset(app, card):
-    return "card3"
+@carousel_app.transition("start", "click", "path1")
 
-def progress(app, card):
+def path1(app, card):
+    return "bombexploding"
+
+@carousel_app.transition("start", "click", "path2")
+
+def path2(app, card):
+    return "timerjump"
+
+#---------------------- Path 1
+
+@carousel_app.transition("bombexploding", "click", "parryB")
+
+def parryB(app, card):
     return "parry"
 
+@carousel_app.transition("bombexploding", "click", "lakeB")
+
+def lakeB(app, card):
+    return "lake"
+
+@carousel_app.transition("bombexploding", "click", "copsB")
+
+def copsB(app, card):
+    return "cops"
+
+#---------------------- Path 2
+
+@carousel_app.transition("timerjump", "click", "madB")
+
+def madB(app, card):
+    return "grounded"
+
+@carousel_app.transition("timerjump", "click", "jumpB")
+
+def jumpB(app, card):
+    return "start"
+
+@carousel_app.transition("timerjump", "click", "unplugB")
+
+def unplugB(app, card):
+    return "mad"
+
+#---------------------- Path 2 End
+
+@carousel_app.transition("mad", "click", "okB")
+
+def okB(app, card):
+    return "ok"
+
+@carousel_app.transition("mad", "click", "voltageB")
+
+def voltageB(app, card):
+    return "voltage"
+
+@carousel_app.transition("mad", "click", "blue")
+
+def blue(app, card):
+    return "bombexploding"
+
+#---------------------- Second jump
+
+@carousel_app.transition("mad", "click", "jumpB")
+
+def jumpB(app, card):
+    return "bombexploding"
 
 carousel_app.start("start")
